070d34d9e0c8c9f9e17925a3b5a8b621ae90e973
Samuel Williams <samueltwilliams@gmail.com>
2012-11-04 18:32:42 +0000

    matching longer paths
    fixed an issue where later parts of a path were dropped

--- test/router.test.js
+++ test/router.test.js
@@ -3,12 +3,19 @@ var makeRouter = require("../lib/tabs")
 
 describe("router", function(){
     it("should trigger callback and get return value", function(){
-        var router    = makeRouter()
+        var router = makeRouter()
         router.add("/bar", function(){
             return "woo, bar triggered"
         })
 
         var triggered = router.match("/bar")
-        expect(triggered).to.be.equal("woo, bar triggered")
+        expect(triggered).to.equal("woo, bar triggered")
+
+        
+        router.add("/bar/foo/farr", function(){
+            return "bar/foo/farr triggered"
+        })
+
+        expect(router.match("/bar/foo/farr")).to.equal("bar/foo/farr triggered")
     })
 })
