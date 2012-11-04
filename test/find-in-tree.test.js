44d9c3c5357cb629f4c4b17b1421a1ee640361bb
Samuel Williams <samueltwilliams@gmail.com>
2012-11-04 12:09:53 +0000

    testing failure cases

--- test/find-in-tree.test.js
+++ test/find-in-tree.test.js
@@ -3,31 +3,24 @@ var findIn = require("../lib/find-in-tree")
 
 describe("Tree Walker", function(){
     describe("finding fixed paths", function(){
-        it("should find /foo/bar", function(){
-            var tree = {
+        var tree = {
                 "fixed" : {
                     "foo" : {
                         "fixed" : {
-                            "bar" : {}
+                            "bar" : { "leaf" : true }
                         }
                     }
                 }
             }
 
+        it("should find /foo/bar", function(){
             findIn(["foo", "bar"], tree).should.eql(true)
         })
-        it("should not find /foo as its incomplete", function(){
-            var tree = {
-                "fixed" : {
-                    "foo" : {
-                        "fixed" : {
-                            "bar" : {}
-                        }
-                    }
-                }
-            }
-
-            findIn(["bar"], tree).should.eql(false)
+        it("should not find /foo as it is incomplete", function(){
+            findIn(["foo"], tree).should.eql(false)
+        })
+        it("should not find /foo/bar/baz as it is too long", function(){
+            findIn(["foo", "bar", "baz"], tree).should.eql(false)
         })
     })
 })
